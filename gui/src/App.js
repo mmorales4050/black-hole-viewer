dfdeb4945dfad9e03c125c1621957d65dfd96d1b
mmorales4050 <mmorales4050@gmail.com>
2019-12-02 17:03:56 -0500

    add 3 column layout

--- gui/src/App.js
+++ gui/src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import './App.css';
+import { Button, Divider, Form, Grid, Header } from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css'
+
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Grid container columns={3} divided relaxed stackable>
+      <Grid.Column>
+      <Header as='h3' textAlign='center'>
+    Input Form
+    </Header>
+      </Grid.Column>
+
+      <Grid.Column verticalAlign='middle'>
+      <Header as='h3' textAlign='center'>
+    Graph
+    </Header>
+      </Grid.Column>
+      <Grid.Column verticalAlign='middle'>
+      <Header as='h3' textAlign='center'>
+    Top 50 Brightest lines
+    </Header>
+      </Grid.Column>
+    </Grid>
   );
 }
 
