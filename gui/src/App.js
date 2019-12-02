d60aaaf7a505938cbfead7c0ac4611bc1e0f366b
mmorales4050 <mmorales4050@gmail.com>
2019-12-02 17:49:30 -0500

    add input form

--- gui/src/App.js
+++ gui/src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
 import './App.css';
-import { Button, Divider, Form, Grid, Header } from 'semantic-ui-react'
+import { Button, Grid, Header, Input } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
 
 
 
 function App() {
   return (
-    <Grid container columns={3} divided relaxed stackable>
-      <Grid.Column>
-      <Header as='h3' textAlign='center'>
+    <Grid container columns={3} divided relaxed stackable style={{"padding-top": "30px"}}>
+      <Grid.Column textAlign='center'>
+      <Header as='h3' >
     Input Form
     </Header>
+    <Input label='AGN' placeholder='' style={{padding: "5px"}}/>
+    <br/>
+    <Input label='Z' placeholder='' style={{padding: "5px"}}/>
+    <br/>
+    <Input label='n' placeholder='' style={{padding: "5px"}}/>
+    <br/>
+    <Input label='NH' placeholder='' style={{padding: "5px"}}/>
+    <br/>
+    <Input label='R' placeholder='' style={{padding: "5px"}}/>
+    <br/>
+    <Button style={{margin: "5px", width: "154px", height: "37px"}}>
+    Submit
+    </Button>
       </Grid.Column>
 
-      <Grid.Column verticalAlign='middle'>
-      <Header as='h3' textAlign='center'>
+      <Grid.Column textAlign='center'>
+      <Header as='h3'>
     Graph
     </Header>
       </Grid.Column>
-      <Grid.Column verticalAlign='middle'>
-      <Header as='h3' textAlign='center'>
+      <Grid.Column textAlign='center'>
+      <Header as='h3'>
     Top 50 Brightest lines
     </Header>
       </Grid.Column>
