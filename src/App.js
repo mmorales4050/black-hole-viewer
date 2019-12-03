9b2b52b8577a49c0d8cfe5481473d4a7232199a6
mmorales4050 <mmorales4050@gmail.com>
2019-12-02 19:49:17 -0500

    add csv parser

--- src/App.js
+++ src/App.js
@@ -1,45 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Papa from 'papaparse';
 import './App.css';
 import { Button, Grid, Header, Input } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
 
 
 
-function App() {
-  return (
-    <Grid container columns={3} divided relaxed stackable style={{"padding-top": "30px"}}>
-      <Grid.Column textAlign='center'>
-      <Header as='h3' >
-    Input Form
-    <a href='data/grid000000000_Z_0p1_n_300_0per.con' download>Click to download</a>
-    </Header>
-    <Input label='AGN' placeholder='' style={{padding: "5px"}}/>
-    <br/>
-    <Input label='Z' placeholder='' style={{padding: "5px"}}/>
-    <br/>
-    <Input label='n' placeholder='' style={{padding: "5px"}}/>
-    <br/>
-    <Input label='NH' placeholder='' style={{padding: "5px"}}/>
-    <br/>
-    <Input label='R' placeholder='' style={{padding: "5px"}}/>
-    <br/>
-    <Button style={{margin: "5px", width: "154px", height: "37px"}}>
-    Submit
-    </Button>
-      </Grid.Column>
 
-      <Grid.Column textAlign='center'>
-      <Header as='h3'>
-    Graph
-    </Header>
-      </Grid.Column>
-      <Grid.Column textAlign='center'>
-      <Header as='h3'>
-    Top 50 Brightest lines
-    </Header>
-      </Grid.Column>
-    </Grid>
-  );
+class App extends Component {
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <Grid container columns={3} divided relaxed stackable style={{"padding-top": "30px"}}>
+        <Grid.Column textAlign='center'>
+        <Header as='h3' >
+      Input Form
+      </Header>
+      <Input label='AGN' placeholder='' style={{padding: "5px"}}/>
+      <br/>
+      <Input label='Z' placeholder='' style={{padding: "5px"}}/>
+      <br/>
+      <Input label='n' placeholder='' style={{padding: "5px"}}/>
+      <br/>
+      <Input label='NH' placeholder='' style={{padding: "5px"}}/>
+      <br/>
+      <Input label='R' placeholder='' style={{padding: "5px"}}/>
+      <br/>
+      <Button style={{margin: "5px", width: "154px", height: "37px"}}>
+      Submit
+      </Button>
+        </Grid.Column>
+
+        <Grid.Column textAlign='center'>
+        <Header as='h3'>
+      Graph
+      </Header>
+        </Grid.Column>
+        <Grid.Column textAlign='center'>
+        <Header as='h3'>
+      Top 50 Brightest lines
+      </Header>
+        </Grid.Column>
+      </Grid>
+    )
+  }
 }
 
 export default App;
