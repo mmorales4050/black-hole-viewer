8456e28310b02ffc508a7288d00d3d0c5603e9f6
mmorales4050 <mmorales4050@gmail.com>
2019-12-06 15:37:43 -0500

    update to graphing data

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -6,28 +6,40 @@ export default class Graph extends Component {
     chartRef = React.createRef();
 
     componentDidMount() {
+      // filter out data that will not be graphed
       let graph = this.props.graph
       graph = graph.split(/(\s+)/).filter((value) => {
         return value.includes("e") && value.length > 6
       })
       graph.shift()
-      console.log(graph)
+      // extract data that will be graphed
+      let data_set = []
+      let data_point = {}
+      let counter = 0
+      let nu_counter = 0
+      let total_counter = 6
+      graph.forEach((value) => {
+        if(counter - nu_counter === 0) {
+          nu_counter += 9
+          data_point.x = Number(value)
+        }
+        if(counter - total_counter === 0) {
+          total_counter += 9
+          data_point.y = Number(value)
+          data_set.push(data_point)
+        }
+        counter ++
+      })
+      console.log(data_set)
         const myChartRef = this.chartRef.current.getContext("2d");
 
         new Chart(myChartRef, {
-            type: "line",
+            type: "scatter",
             data: {
-                //Bring in data
                 datasets: [
                     {
-                      fill: false,
-                        label: "NU",
-                        data: [],
-                    },
-                    {
-                      fill: false,
-                      label: "REFLC",
-                      data: [],
+                      label: 'Scatter Dataset',
+                      data: data_set
                     }
                 ]
             },
@@ -39,7 +51,7 @@ export default class Graph extends Component {
                   }],
                   yAxes: [{
                     display: true,
-                    type: 'logarithmic'
+                    type: 'linear'
                   }]
                 }
             }
@@ -56,3 +68,58 @@ export default class Graph extends Component {
         )
     }
 }
+
+
+	var color = Chart.helpers.color;
+	var scatterChartData = {
+		datasets: [{
+			borderColor: window.chartColors.red,
+			backgroundColor: color(window.chartColors.red).alpha(0.5).rgbString(),
+			label: 'V(node2)',
+			data: []
+		}]
+	};
+
+	window.onload = function() {
+		var ctx = document.getElementById('canvas').getContext('2d');
+		window.myScatter = Chart.Scatter(ctx, {
+			data: scatterChartData,
+			options: {
+				title: {
+					display: true,
+					text: 'Chart.js Scatter Chart - Logarithmic X-Axis'
+				},
+				scales: {
+					xAxes: [{
+						type: 'logarithmic',
+						position: 'bottom',
+						ticks: {
+							userCallback: function(tick) {
+								var remain = tick / (Math.pow(10, Math.floor(Chart.helpers.log10(tick))));
+								if (remain === 1 || remain === 2 || remain === 5) {
+									return tick.toString() + 'Hz';
+								}
+								return '';
+							},
+						},
+						scaleLabel: {
+							labelString: 'Frequency',
+							display: true,
+						}
+					}],
+					yAxes: [{
+						type: 'linear',
+						ticks: {
+							userCallback: function(tick) {
+								return tick.toString() + 'dB';
+							}
+						},
+						scaleLabel: {
+							labelString: 'Voltage',
+							display: true
+						}
+					}]
+				}
+			}
+		});
+	};
