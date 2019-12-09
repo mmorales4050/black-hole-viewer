5a148a0411dafb21e3aa7fc575851fa02bbd8914
mmorales4050 <mmorales4050@gmail.com>
2019-12-09 17:36:43 -0500

    bug fix

--- src/components/GraphContainer.js
+++ src/components/GraphContainer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Button} from 'semantic-ui-react'
+import {Button, Header} from 'semantic-ui-react'
 import Graph from './Graph'
 import Graph1 from './Graph1'
 
