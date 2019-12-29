b82a12a2e1dac0c3bb6ae66fa202c0b7b66928f0
mmorales4050 <mmorales4050@gmail.com>
2019-12-28 19:30:35 -0500

    fixed state in functional component

--- src/components/PersistentDrawerLeft.js
+++ src/components/PersistentDrawerLeft.js
@@ -19,6 +19,7 @@ import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
 import TemporaryDrawer from './TemporaryDrawer'
 import WelcomePage from './WelcomePage'
+import Papa from 'papaparse';
 
 const drawerWidth = 240;
 
@@ -84,25 +85,126 @@ const useStyles = makeStyles(theme => ({
 export default function PersistentDrawerLeft() {
   const classes = useStyles();
   const theme = useTheme();
-  const [state, setState] = React.useState({
-    open: false,
-    inputOpen: false,
-    comFile: null,
-    graphFile: null,
-    selection: [], // Used to display top 50 brightest lines
-    data: [] // Data for graph
+  const useEffect = React.useEffect
+  const useState = React.useState
+  const [open, setOpen] = useState(false)
+  const [inputOpen, setInputOpen] = useState(false)
+  const [comFile, setComFile] = useState(null)
+  const [graphFile, setGraphFile] = useState(null)
+  const [selection, setSelection] = useState([]) // Used to display top 50 brightest lines
+  const [data, setData] = useState([]) // Data for graph
+
+
+  const updateApp = (inputState) => {
+    let selection = comFile.data.filter((item) => {
+      return parseFloat(item[1]) === parseFloat(inputState.agn) && parseFloat(item[2]) === parseFloat(inputState.z) && parseFloat(item[3]) === parseFloat(inputState.n) && parseFloat(item[4]) === parseFloat(inputState.r) && parseFloat(item[5]) === parseFloat(inputState.nh)
+    })
+    if(selection.length === 0) {
+      // Warn user that input is invalid
+      console.log("invalid input")
+    }else {
+      console.log("Graph updated")
+      // update graph and top 50 brightest lines
+      let file = fileName(selection[0])
+      setSelection(selection[0])
+      setGraphFile(file)
+      getData(file)
+    }
+  }
+
+  const fileName = (selection) => {
+    // Create file name from selection
+    let zeros = 9 - selection[0].length
+    // debugger
+    let file = "grid" + "0".repeat(zeros) + selection[0]
+    if(selection[2] === "1") {
+      file += "_Z_1_n_"
+    } else {
+      file += "_Z_0p1_n_"
+    }
+    file += selection[3] + "_" + selection[1] + "per.con"
+    // Set configuration file to state
+    return ("data/" + file)
+  }
+
+  const getData = file => {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', file , true);
+    // If specified, responseType must be empty string or "text"
+    xhr.responseType = 'text';
+
+    xhr.onload = () => {
+      if (xhr.readyState === xhr.DONE && this.state.graphContents !== xhr.responseText) {
+          if (xhr.status === 200) {
+              this.setData(extractData(xhr.responseText))
+            }
+          }
+        }
+        xhr.send(null);
+  }
+
+  const extractData = graph => {
+    graph = graph.split(/(\s+)/).filter((value) => {
+      return value.includes("e") && value.length > 6 && !value.includes("i")
+    })
+
+    // extract data that will be graphed
+    let data_set = []
+    let data_point = {}
+    let counter = 0
+    let nu_counter = 0
+    let total_counter = 6
+    let max_x = 0
+    let min_x = graph[0]
+    let min_y = graph[6]
+    let max_y = 0
+    graph.forEach((value) => {
+      if(counter - nu_counter === 0) {
+        nu_counter += 9
+        data_point.x = Number(value)
+        if(Number(value) > max_x) {
+          max_x = Number(value)
+        }
+        if(Number(value) < min_x) {
+          min_x = Number(value)
+        }
+      }
+      if(counter - total_counter === 0) {
+        total_counter += 9
+        data_point.y = Number(value)
+        data_set.push([data_point.x, data_point.y])
+        data_point = {}
+        if(Number(value) > max_y) {
+          max_y = Number(value)
+        }
+        if(Number(value) < min_y) {
+          min_y = Number(value)
+        }
+      }
+      counter ++
+    })
+    return data_set
+  }
+
+  useEffect(() => {
+    Papa.parse("combinedFile.csv", {
+      download: true,
+      complete: (results) => {
+          setComFile(results)
+       }
+    })
   })
 
   const toggleInput = (open) => {
-    setState({...state, inputOpen: open})
+    setInputOpen(open)
   }
 
   const handleDrawerOpen = () => {
-    setState({...state, open: true})
+    setOpen(true)
   };
 
   const handleDrawerClose = () => {
-    setState({...state, open: false})
+    setOpen(false)
   };
 
   const drawer = () => {
@@ -110,7 +212,7 @@ export default function PersistentDrawerLeft() {
       className={classes.drawer}
       variant="persistent"
       anchor="left"
-      open={state.open}
+      open={open}
       classes={{
         paper: classes.drawerPaper,
       }}
@@ -147,7 +249,7 @@ export default function PersistentDrawerLeft() {
       <AppBar
         position="fixed"
         className={clsx(classes.appBar, {
-          [classes.appBarShift]: state.open,
+          [classes.appBarShift]: open,
         })}
       >
         <Toolbar>
@@ -156,20 +258,22 @@ export default function PersistentDrawerLeft() {
             aria-label="open drawer"
             onClick={handleDrawerOpen}
             edge="start"
-            className={clsx(classes.menuButton, state.open && classes.hide)}
+            className={clsx(classes.menuButton, open && classes.hide)}
           >
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" noWrap className={classes.title}>
             Graph
           </Typography>
-          <TemporaryDrawer inputOpen={state.inputOpen} toggleInput={toggleInput}/>
+          <TemporaryDrawer
+            updateApp={updateApp}
+            inputOpen={inputOpen} toggleInput={toggleInput}/>
         </Toolbar>
       </AppBar>
       {drawer()}
       <main
         className={clsx(classes.content, {
-          [classes.contentShift]: state.open,
+          [classes.contentShift]: open,
         })}
       >
         <div className={classes.drawerHeader} />
